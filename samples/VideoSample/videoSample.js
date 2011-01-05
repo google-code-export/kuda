2fa0f27c532cfe2fc4f7d8997058cc865b3e47ab
Stuart Mershon <stuart.mershon@gmail.com>
2011-01-05 22:19:58 +0000

    Put license header on js, css and html files in hemi, hext, editor and samples. GPL2 LICENSE placed at root.

--- samples/VideoSample/videoSample.js
+++ samples/VideoSample/videoSample.js
@@ -1,4 +1,21 @@
-/**
+/* 
+ * Kuda includes a library and editor for authoring interactive 3D content for the web.
+ * Copyright (C) 2011 SRI International.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation; either 
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; 
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ * Boston, MA 02110-1301 USA.
+ */
+ 
+ /**
  * This is Tutorial A using Kuda 1.0.  It shows how to set up a simple world, 
  *		load a model, and set the camera to a viewpoint once the model
  *		has loaded.  The script called for the user interactions on the door
