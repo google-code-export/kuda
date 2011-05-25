d2ab0bfeb529b4b03187922d9b5400742bc0d98c
Khang Duong <khang.duong@gmail.com>
2011-05-24 17:40:33 -0700

    css fixes.

--- public/js/editor/tools/hudDisplays.js
+++ public/js/editor/tools/hudDisplays.js
@@ -273,7 +273,7 @@ var editor = (function(module) {
 			this.instructions = jQuery("<p>Type in a name and click 'Create New Display' to add a new HUD display box</p>");
 			this.form = jQuery('<form method="post"></form>');
 			this.nameInput = jQuery('<input type="text" id="hudDpyName" />');
-			this.createBtn = jQuery('<button id="createHudDisplay">Create New Display</button>');
+			this.createBtn = jQuery('<button id="createHudDisplay" class="inlineBtn">Create New Display</button>');
 			var wgt = this;
 			
 			this.createBtn.bind('click', function(evt) {
