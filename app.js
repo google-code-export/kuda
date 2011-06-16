e2208fe8bcab2e2451a22267e65a458445154e57
Erik Kitson <erik.kitson@gmail.com>
2011-06-16 11:24:46 -0700

    Issue 39: Initial publish feature added to World Editor. Updated build script to filter out new publish files.

---
+++ app.js
@@ -0,0 +1,240 @@
+
+/**
+ * Module dependencies.
+ */
+var express = require('express'),
+	child = require('child_process'),
+	fs = require('fs'),
+	util = require('util'),
+	path = require('path');
+
+var app = module.exports = express.createServer(),
+	projectsPath = 'public/projects';
+
+// Configuration
+
+app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser());
+    app.use(express.session({
+        secret: 'your secret here'
+    }));
+    app.use(express.compiler({
+        src: __dirname + '/public',
+        enable: ['sass']
+    }));
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler());
+});
+
+// Routes
+
+//app.get('/', function(req, res){
+//  res.render('index', {
+//    title: 'Express'
+//  });
+//});
+
+app.get('/listProjects', function(req, res) {
+	if (req.isXMLHttpRequest) {
+		var data = {
+			options: []
+		};
+		
+		if (!path.existsSync(projectsPath)) {
+			fs.mkdirSync(projectsPath, 0755);
+		} else {
+			var files = fs.readdirSync(projectsPath);
+			
+			for (var ndx = 0, len = files.length; ndx < len; ndx++) {				
+				var file = files[ndx];
+				
+				if (file.match('.json')) {
+					file = file.split('.')[0];
+					data.options.push(file);	
+				}
+			}
+		}
+		
+		res.send(data, 200);		
+	}
+});
+
+app.post('/saveProject', function(req, res) {	
+	if (req.isXMLHttpRequest) {		
+		if (!path.existsSync(projectsPath)) {
+			fs.mkdirSync(projectsPath, 0755);
+		}
+		
+		var defName = 'project',			
+			name = req.param('name', defName) + '.json',
+			replace = req.param('replace') == 'true',
+			filePath = projectsPath + '/' + name;
+		
+		if (path.existsSync(filePath) && !replace) {
+			var oldData = {
+				name: req.param('name'),
+				octane: req.param('octane')
+			};
+			
+			res.send({
+				errType: 'fileExists',
+				errData: oldData,
+				errMsg: 'File by that name already exists'
+			}, 400);
+		}
+		else {
+			var input = req.param('octane');
+			
+			fs.writeFileSync(filePath, input);
+			res.send({
+				name: name
+			}, 200);
+		}
+	}
+});
+
+app.get('/openProject', function(req, res) {
+	if (req.isXMLHttpRequest) {
+		var name = req.param('name') + '.json',
+			filePath = projectsPath + '/' + name;
+		
+		if (path.existsSync(filePath)) {			
+			var data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+			res.send(data, 200);
+		}
+		else {
+			res.send('File named ' + name + ' does not exist', 400);
+		}
+	}
+});
+
+app.post('/publishProject', function(req, res) {
+	if (req.isXMLHttpRequest) {
+		var name = req.param('name'),
+			models = req.param('models'),
+			filePath = projectsPath + '/' + name + '.html',
+			content = fs.readFileSync('PublishTemplate.html', 'utf8'),
+			readme = fs.readFileSync('PublishReadMe', 'utf8'),
+			start = content.indexOf('<!DOCTYPE');
+		
+		content = content.substr(start);
+		fs.writeFileSync(filePath,
+			content.replace(/%PROJECT%/g, 'projects/' + name)
+				.replace(/%LOAD%/g, '..').replace(/%SCRIPT%/g, '../../js'));
+		
+		// Create the published package
+		var toDir = projectsPath + '/' + name;
+		var stat = fs.statSync(projectsPath);
+		fs.mkdirSync(toDir, stat.mode);
+		fs.mkdirSync(toDir + '/assets', stat.mode);
+		fs.mkdirSync(toDir + '/lib', stat.mode);
+		copyFiles('./public/js/hemi', toDir + '/hemi');
+		copyFiles('./public/js/o3d-webgl', toDir + '/o3d-webgl');
+		copyFiles('./public/js/o3djs', toDir + '/o3djs');
+		var data = fs.readFileSync('./public/js/lib/jshashtable.js');
+		fs.writeFileSync(toDir + '/lib/jshashtable.js', data);
+		data = fs.readFileSync('./public/js/lib/JSON.js');
+		fs.writeFileSync(toDir + '/lib/JSON.js', data);
+		data = fs.readFileSync('./public/js/lib/JSONError.js');
+		fs.writeFileSync(toDir + '/lib/JSONError.js', data);
+		data = fs.readFileSync(projectsPath + '/' + name + '.json');
+		fs.writeFileSync(toDir + '/README', readme.concat(models));
+		fs.writeFileSync(toDir + '/' + name + '.json', data);
+		fs.writeFileSync(toDir + '/' + name + '.html',
+			content.replace(/%PROJECT%/g, name).replace(/%LOAD%/g, '.')
+				.replace(/%SCRIPT%/g, '.'));
+		
+		// Compress the package and remove the files
+		var tarChild = child.spawn('tar', ['-czf', name + '.tgz', name],
+			{cwd: projectsPath});
+		
+		tarChild.on('exit', function (code) {
+			removeFiles(toDir);
+			res.send({
+				name: name + '.html'
+			}, code === 0 ? 200 : 500);
+		});
+	}
+});
+
+var copyFiles = function(fromDir, toDir) {
+	var files = [],
+		dirs = [];
+	
+	getDirContents(fromDir, files, dirs);
+	
+	if (!path.existsSync(toDir)) {
+		var stat = fs.statSync(fromDir);
+		fs.mkdirSync(toDir, stat.mode);
+	}
+	
+	for (var i = 0, il = files.length; i < il; i++) {
+		var file = files[i],
+			data = fs.readFileSync(file),
+			newFile = toDir + '/' + path.basename(file);
+		
+		fs.writeFileSync(newFile, data);
+	}
+	
+	for (var i = 0, il = dirs.length; i < il; i++) {
+		var dir = '/' + path.basename(dirs[i]);
+		copyFiles(fromDir + dir, toDir + dir);
+	}
+};
+
+var getDirContents = function(dir, files, dirs) {
+	var dirFiles = fs.readdirSync(dir);
+	
+	for (var i = 0, il = dirFiles.length; i < il; i++) {
+		var file = dirFiles[i],
+			fPath = path.resolve(dir, file),
+			stat = fs.statSync(fPath);
+		
+		if (stat.isDirectory()) {
+			dirs.push(fPath);
+		} else if (stat.isFile()) {
+			files.push(fPath);
+		}
+	}
+};
+
+var removeFiles = function(dir) {
+	var files = [],
+		dirs = [];
+	
+	getDirContents(dir, files, dirs);
+	
+	for (var i = 0, il = dirs.length; i < il; i++) {
+		var subDir = '/' + path.basename(dirs[i]);
+		removeFiles(dir + subDir);
+	}
+	
+	for (var i = 0, il = files.length; i < il; i++) {
+		fs.unlinkSync(files[i]);
+	}
+	
+	fs.rmdirSync(dir);
+};
+
+
+// Only listen on $ node app.js
+
+if (!module.parent) {
+    app.listen(3000);
+    console.log("Express server listening on port %d", app.address().port);
+}
---
+++ build.js
@@ -0,0 +1,166 @@
+var child = require('child_process'),
+	fs = require('fs'),
+	path = require('path'),
+	procFds = [process.stdin.fd, process.stdout.fd, process.stderr.fd],
+	filter = [];
+
+var createAssetsDir = function(fromDir, toDir) {
+	if (!path.existsSync(toDir)) {
+		var stat = fs.statSync(fromDir);
+		fs.mkdirSync(toDir, stat.mode);
+	}
+	
+	var data = fs.readFileSync(fromDir + '/LICENSE.cc'),
+		newFile = toDir + '/LICENSE.cc';
+	
+	fs.writeFileSync(newFile, data);
+	data = fs.readFileSync(fromDir + '/NOTICES'),
+	newFile = toDir + '/NOTICES';
+	fs.writeFileSync(newFile, data);
+};
+
+var copyFiles = function(fromDir, toDir, subDirs) {
+	var files = [],
+		dirs = [];
+	
+	getDirContents(fromDir, files, dirs);
+	
+	if (!path.existsSync(toDir)) {
+		var stat = fs.statSync(fromDir);
+		fs.mkdirSync(toDir, stat.mode);
+	}
+	
+	for (var i = 0, il = files.length; i < il; i++) {
+		var file = files[i],
+			data = fs.readFileSync(file),
+			newFile = toDir + '/' + path.basename(file);
+		
+		fs.writeFileSync(newFile, data);
+	}
+	
+	if (subDirs) {
+		for (var i = 0, il = dirs.length; i < il; i++) {
+			var dir = '/' + path.basename(dirs[i]);
+			copyFiles(fromDir + dir, toDir + dir, subDirs);
+		}
+	}
+};
+
+var getDirContents = function(dir, files, dirs) {
+	var dirFiles = fs.readdirSync(dir);
+	
+	for (var i = 0, il = dirFiles.length; i < il; i++) {
+		var file = dirFiles[i],
+			fName = path.basename(file),
+			fPath = path.resolve(dir, file),
+			stat = fs.statSync(fPath);
+		
+		if (filterFile(fName)) {
+			if (stat.isDirectory()) {
+				dirs.push(fPath);
+			} else if (stat.isFile()) {
+				files.push(fPath);
+			}
+		}
+	}
+};
+
+var filterFile = function(file) {
+	return filter.indexOf(file) === -1;
+};
+
+var removeFiles = function(dir) {
+	var files = [],
+		dirs = [];
+	
+	getDirContents(dir, files, dirs);
+	
+	for (var i = 0, il = dirs.length; i < il; i++) {
+		var subDir = '/' + path.basename(dirs[i]);
+		removeFiles(dir + subDir);
+	}
+	
+	for (var i = 0, il = files.length; i < il; i++) {
+		fs.unlinkSync(files[i]);
+	}
+	
+	fs.rmdirSync(dir);
+};
+
+var compressDir = function(toDir) {
+	// Package and compress the created directory
+	var tarChild = child.spawn('tar', ['-czf', toDir + '.tgz', toDir],
+		{customFds: procFds});
+	
+	tarChild.on('exit', function (code) {
+		if (code === 0) {
+			// Clean up the created directory
+			filter = [];
+			removeFiles(toDir);
+		}
+	});
+};
+
+if (process.argv.length > 3) {
+	var ndx = 2,
+	docs = true,
+	compress = true;
+	// Check for flags
+	while(process.argv[ndx].substr(0, 2) === '--') {
+		var flag = process.argv[ndx++];
+		
+		switch (flag) {
+			case '--no-doc':
+				docs = false;
+				break;
+			case '--no-zip':
+				compress = false;
+				break;
+		}
+	}
+	// Get build arguments
+	var type = process.argv[ndx++],
+		toDir = process.argv[ndx];
+	
+	if (path.existsSync(toDir)) {
+		process.stdout.write('Cannot write to ' + toDir + ': already exists\n');
+		process.exit(-1);
+	}
+	// Set up our filter
+	filter = ['.svn', '.hg', '.hgignore', '.hgtags', '.project', '.settings'];
+	
+	if (type === 'core') {
+		// Copy only the core Hemi library
+		filter.push('app.js', 'build.js', 'PublishReadMe',
+			'PublishTemplate.html', 'editor');
+		copyFiles('.', toDir, false);
+		copyFiles('./public/js', toDir, true);
+        
+        if (docs) {
+            copyFiles('./public/doc', toDir + '/doc', true);
+        }
+	} else {
+		// Unless building a full package, do not copy samples
+		if (type !== 'full') {
+			filter.push('samples', 'assets');
+		}
+		if (!docs) {
+			filter.push('doc');
+		}
+		// Now copy the Kuda files
+		copyFiles('.', toDir, true);
+		// If not building a full package, create an empty assets directory
+		if (type !== 'full') {
+			createAssetsDir('public/assets', toDir + '/public/assets');
+		}
+	}
+	
+	// Optionally compress
+	if (compress) {
+		compressDir(toDir);
+	}
+} else {
+	process.stdout.write('Usage: node build.js [options] [type] [toDir]\n' +
+		'Valid options are: --no-doc, --no-zip\n' +
+		'Valid types are: core, editor, full\n');
+}
