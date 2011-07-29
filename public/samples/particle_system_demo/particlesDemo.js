b7c87df9b52c20693a9112d4925185e8c956d5b3
Erik Kitson <erik.kitson@gmail.com>
2011-07-29 14:53:25 -0700

    Issue 48: Camera's moveToView and moveOnCurve now only accept the second parameter as seconds. Updated documentation various functions to be more clear. Updated samples for change.

--- public/samples/particle_system_demo/particlesDemo.js
+++ public/samples/particle_system_demo/particlesDemo.js
@@ -45,7 +45,7 @@
 		vp.eye = [-10,800,1800];					// Set viewpoint eye
 		vp.target = [10,250,30];					// Set viewpoint target
 
-		hemi.world.camera.moveToView(vp,60);
+		hemi.world.camera.moveToView(vp, 2.5);
 		
 		hemi.world.camera.enableControl();	// Enable camera mouse control
 		
