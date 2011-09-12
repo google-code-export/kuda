176a4baf33e864615e46203156fe7b2f95dba36c
Erik Kitson <erik.kitson@gmail.com>
2011-09-12 10:54:16 -0700

    Fixed name of behavior tool model and view. Removed createListWidget from manips.

--- public/js/editor/plugins/manips/manips.js
+++ public/js/editor/plugins/manips/manips.js
@@ -598,10 +598,6 @@
 			});
 		},
 		
-		createListItemWidget: function() {
-			return new editor.ui.BhvListItemWidget();
-		},
-		
 		getOtherHeights: function() {
 			return this.buttonDiv.outerHeight(true);
 		}
