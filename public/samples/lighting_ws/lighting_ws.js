bfb9fb263f706bb40bf74e817ba735267bcfb949
Scott Kuroda <scott.kuroda@gmail.com>
2011-10-28 15:18:59 -0700

    Move WebSocket handling into closure.  Removed reliance on unnecessary Websocket object.

--- public/samples/lighting_ws/lighting_ws.js
+++ public/samples/lighting_ws/lighting_ws.js
@@ -20,8 +20,10 @@
  */
 
 (function() {
-	var	houseModel;
-
+	var	houseModel,
+        textureSet = {},
+        emissiveSamplers = {};
+        
 	function initStep(clientElements) {
 		hemi.core.init(clientElements[0]);
 		hemi.view.setBGColor([1, 1, 1, 1]);
@@ -51,8 +53,7 @@
 		viewpoint.target = [160.0, 100.0, 0.0];
 		hemi.world.camera.moveToView(viewpoint, 1);
 		hemi.world.camera.subscribe(hemi.msg.stop,function(){
-            var client = new Websocket();
-            client.connect();
+            connect();
         });
 	}
 	
@@ -76,115 +77,99 @@
 			hemi.core.client.cleanup();
 		}
 	});
+    // WebSockets handling
+
+        
+    function connect() {
+        var url = "ws://" + document.URL.substr(7).split('/')[0];
+        var wsCtor = window['MozWebSocket'] ? MozWebSocket : WebSocket;
+        this.socket = new wsCtor(url, 'websocket-lighting');
+
+        this.socket.onmessage = handleWebsocketMessage.bind(this);
+        this.socket.onclose = handleWebsocketClose.bind(this);
+    };
+
+    function handleWebsocketMessage (message) {
+        try {
+            var command = JSON.parse(message.data);
+        }
+        catch(e) { 
+            console.log('Error on JSON parse');
+            console.log(e);
+            console.log(message);
+        }
+
+        textureImage(command);
+    };
+
+    function handleWebsocketClose () {
+        alert("WebSocket Connection Closed.");
+    };
+
+
+    function textureImage (command) {
+        var data = command.data;
+        // Get texture name
+        var setName =data.setName;
+        var textureName = data.textureName;
+        // Create texture
+        var pack = hemi.core.mainPack;
+        var rawData = new o3d.RawData();
+        rawData.image_ = new Image();
+        rawData.image_.src = data.img;
+        rawData.image_.onload = function() {
+            // When loading, create textureSampler and load into a textureSet
+            var texture = o3djs.texture.createTextureFromRawData(pack,rawData);
+            var textureSampler = new hemi.texture.TextureSampler();
+            textureSampler.value.texture = texture;
+            if (setName in textureSet) {
+                textureSet[setName].addTextureSampler(textureName,textureSampler);
+                textureSet[setName].samplers.length =  textureSet[setName].samplers.length  + 1;
+                if (textureSet[setName].samplers.length == 14) {
+                        createButton(setName);
+                }
+            } else {
+                textureSet[setName] = new hemi.texture.TextureSet();
+                textureSet[setName].addTextureSampler(textureName,textureSampler);        
+                textureSet[setName].samplers.length =  textureSet[setName].samplers.length  + 1;
+            }    
+            return;
+        }
+    }
+
+    function createButton(setName) {
+        //Create an input type dynamically.
+        var element = document.createElement("input");
+     
+        //Assign different attributes to the element.
+        element.setAttribute("type", 'button');
+        element.setAttribute("value", setName);
+        element.setAttribute("name", setName);
+        element.onclick = function() {
+            changeModelSamplers(textureSet[setName].samplers);
+        };
+        var buttonDiv = document.getElementById("buttons");
+     
+        //Append the element in page (in span).
+        buttonDiv.appendChild(element); 
+    }
+        
+    function changeModelSamplers(samplers) {
+        // Note: value of a "Sampler", or "Sampler2D", the "emmissiveSampler" is write only.
+        emissiveSamplers.Walls_S.value = samplers.walls.value;
+        emissiveSamplers.Logs_S.value = samplers.logs.value;
+        emissiveSamplers.Kitchen_S.value = samplers.kitchen.value;
+        emissiveSamplers.Ceiling_S.value = samplers.ceiling.value;
+        emissiveSamplers.Bath_S.value = samplers.ba.value;
+        emissiveSamplers.B1_S.value = samplers.b1.value;
+        emissiveSamplers.B2_S.value = samplers.b2.value;
+        emissiveSamplers.Floor_S.value = samplers.floor.value;
+        emissiveSamplers.Win_South_S.value = samplers.window_south.value;
+        emissiveSamplers.Win_LR3_S.value = samplers.window_lr3.value;
+        emissiveSamplers.Win_B2_S.value = samplers.window_b2.value;
+        emissiveSamplers.Win_B1_S.value = samplers.window_b1.value;
+        emissiveSamplers.Win_KI_S.value = samplers.window_ki.value;
+        emissiveSamplers.Win_LR4_S.value = samplers.window_lr4.value;
+    }
 })();
 
-// WebSockets handling
-var textureSet = {},
-    emissiveSamplers = {};
-    
-function Websocket() {    
-    // Define accepted commands
-    this.messageHandlers = {
-        textureImage: this.textureImage.bind(this),        
-    };
-};
-
-Websocket.prototype.connect = function() {
-    var url = "ws://" + document.URL.substr(7).split('/')[0];
-    var wsCtor = window['MozWebSocket'] ? MozWebSocket : WebSocket;
-    this.socket = new wsCtor(url, 'websocket-lighting');
-
-    this.socket.onmessage = this.handleWebsocketMessage.bind(this);
-    this.socket.onclose = this.handleWebsocketClose.bind(this);
-};
-
-Websocket.prototype.handleWebsocketMessage = function(message) {
-    try {
-        var command = JSON.parse(message.data);
-    }
-    catch(e) { 
-        console.log('Error on JSON parse');
-    }
-
-    if (command) {
-        this.dispatchCommand(command);
-    }
-};
-
-Websocket.prototype.handleWebsocketClose = function() {
-    alert("WebSocket Connection Closed.");
-};
-
-Websocket.prototype.dispatchCommand = function(command) {
-    // Do we have a handler function for this command?
-    var handler = this.messageHandlers[command.msg];
-
-    if (typeof(handler) === 'function') {
-        // If so, call it and pass the parameter data
-        handler.call(this, command.data);
-    }
-};
-
-Websocket.prototype.textureImage = function(data) {
-    // Get texture name
-    var setName =data.setName;
-    var textureName = data.textureName;
-    // Create texture
-    var pack = hemi.core.mainPack;
-    var rawData = new o3d.RawData();
-    rawData.image_ = new Image();
-    rawData.image_.src = data.img;
-    rawData.image_.onload = function() {
-        // When loading, create textureSampler and load into a textureSet
-        var texture = o3djs.texture.createTextureFromRawData(pack,rawData);
-        var textureSampler = new hemi.texture.TextureSampler();
-        textureSampler.value.texture = texture;
-        if (setName in textureSet) {
-            textureSet[setName].addTextureSampler(textureName,textureSampler);
-            textureSet[setName].samplers.length =  textureSet[setName].samplers.length  + 1;
-            if (textureSet[setName].samplers.length == 14) {
-                    createButton(setName);
-            }
-        } else {
-            textureSet[setName] = new hemi.texture.TextureSet();
-            textureSet[setName].addTextureSampler(textureName,textureSampler);        
-            textureSet[setName].samplers.length =  textureSet[setName].samplers.length  + 1;
-        }    
-        return;
-    }
-}
-
-function createButton(setName) {
-    //Create an input type dynamically.
-    var element = document.createElement("input");
- 
-    //Assign different attributes to the element.
-    element.setAttribute("type", 'button');
-    element.setAttribute("value", setName);
-    element.setAttribute("name", setName);
-    element.onclick = function() {
-        changeModelSamplers(textureSet[setName].samplers);
-    };
-    var buttonDiv = document.getElementById("buttons");
- 
-    //Append the element in page (in span).
-    buttonDiv.appendChild(element); 
-}
-    
-function changeModelSamplers(samplers) {
-    // Note: value of a "Sampler", or "Sampler2D", the "emmissiveSampler" is write only.
-    emissiveSamplers.Walls_S.value = samplers.walls.value;
-    emissiveSamplers.Logs_S.value = samplers.logs.value;
-    emissiveSamplers.Kitchen_S.value = samplers.kitchen.value;
-    emissiveSamplers.Ceiling_S.value = samplers.ceiling.value;
-    emissiveSamplers.Bath_S.value = samplers.ba.value;
-    emissiveSamplers.B1_S.value = samplers.b1.value;
-    emissiveSamplers.B2_S.value = samplers.b2.value;
-    emissiveSamplers.Floor_S.value = samplers.floor.value;
-    emissiveSamplers.Win_South_S.value = samplers.window_south.value;
-    emissiveSamplers.Win_LR3_S.value = samplers.window_lr3.value;
-    emissiveSamplers.Win_B2_S.value = samplers.window_b2.value;
-    emissiveSamplers.Win_B1_S.value = samplers.window_b1.value;
-    emissiveSamplers.Win_KI_S.value = samplers.window_ki.value;
-    emissiveSamplers.Win_LR4_S.value = samplers.window_lr4.value;
-}
