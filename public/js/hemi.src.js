6c08244eed165f5b8303a397bcd62121419717bc
Peter Blasco <pblasco@gmail.com>
2011-11-18 15:12:23 -0800

    Initial checkin of picking support.  Pick messages will be sent out with a reference to the picked Object3d.

--- public/js/hemi.src.js
+++ public/js/hemi.src.js
@@ -4887,6 +4887,7 @@ var hemi = (function(hemi) {
 	hemi.ModelBase = function(client) {
 		this.client = client;
 		this.fileName = null;
+        this.root = null;
 	};
 	
 	hemi.ModelBase.prototype = {
@@ -4894,11 +4895,11 @@ var hemi = (function(hemi) {
 			var that = this;
 			
 			hemi.loadCollada(this.fileName, function (collada) {
-				var dae = collada.scene;
-				that.client.scene.add(dae);
+				root = collada.scene;
+				that.client.scene.add(root);
 				
 				if (callback) {
-					callback(dae);
+					callback(root);
 				}
 			});
 		},
@@ -4906,7 +4907,24 @@ var hemi = (function(hemi) {
 		setFileName: function(fileName, callback) {
 			this.fileName = fileName;
 			this.load(callback);
-		}
+		},
+
+        getObject3Ds: function(name) {
+            var obj3ds = [];
+            this.getObject3DsRecursive(name, root, obj3ds);
+            return obj3ds;
+        },
+
+        getObject3DsRecursive : function(name, obj3d, returnObjs) {
+            for (var i = 0; i < obj3d.children.length; ++i) {
+                var child = obj3d.children[i];
+                if (child.name == name) {
+                    returnObjs.push(child);
+                }
+
+                this.getObject3DsRecursive(name, child, returnObjs)
+            }
+        }
 	};
 	
 	hemi.makeCitizen(hemi.ModelBase, 'hemi.Model', {
@@ -4916,7 +4934,66 @@ var hemi = (function(hemi) {
 
 	return hemi;
 })(hemi || {});
-/* 
+/*
+ * Kuda includes a library and editor for authoring interactive 3D content for the web.
+ * Copyright (C) 2011 SRI International.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ */
+
+var hemi = (function(hemi) {
+
+    hemi.Picker = function(scene, camera, width, height) {
+        this.scene = scene;
+        this.camera = camera;
+        this.width = width;
+        this.height = height;
+
+        this.projector = new THREE.Projector();
+
+        hemi.input.addMouseDownListener(this);
+    };
+
+    hemi.Picker.prototype = {
+		onMouseDown : function(mouseEvent) {
+            var x = (mouseEvent.x / this.width) * 2 - 1;
+            var y = -(mouseEvent.y / this.height) * 2 + 1;
+            var projVector = new THREE.Vector3(x, y, 0.5);
+
+            this.projector.unprojectVector(projVector, this.camera.threeCamera);
+            var ray = new THREE.Ray(this.camera.threeCamera.position, projVector.subSelf(this.camera.threeCamera.position).normalize());
+
+            var pickedObjs = ray.intersectScene(this.scene);
+
+            if (pickedObjs.length > 0) {
+                hemi.send(hemi.msg.pick,
+					{
+						mouseEvent: mouseEvent,
+                        //PABNOTE right now return the parent of the picked obj
+                        //due to how the loader works
+						pickedMesh: pickedObjs[0].object.parent
+				    });
+            }
+        },
+
+        resize : function(width, height) {
+            this.width = width;
+            this.height = height;
+        }
+    };
+
+    return hemi;
+})(hemi || {});/* 
  * Kuda includes a library and editor for authoring interactive 3D content for the web.
  * Copyright (C) 2011 SRI International.
  *
@@ -4945,6 +5022,8 @@ var hemi = (function(hemi) {
 		this.scene.add(this.camera.light);
 		//this.scene.add(this.light);
         hemi.input.init(renderer.domElement);
+        var dom = this.renderer.domElement;
+        this.picker = new hemi.Picker(this.scene, this.camera, dom.clientWidth, dom.clientHeight);
 	};
 	
 	hemi.Client.prototype = {
@@ -4979,6 +5058,7 @@ var hemi = (function(hemi) {
 			this.renderer.setSize(width, height);
 			this.camera.threeCamera.aspect = width / height;
 			this.camera.threeCamera.updateProjectionMatrix();
+            this.picker.resize(width, height);
 		},
 		
 		setBGColor: function(hex, opt_alpha) {
