fce574707ed06854bdb39fc1b3cc2a215d30546a
Erik Kitson <erik.kitson@gmail.com>
2011-12-23 11:48:17 -0800

    Cleaned up console and core.

--- public/js/hemi/core.js
+++ public/js/hemi/core.js
@@ -1,45 +1,29 @@
-/* Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php */
 /*
-The MIT License (MIT)
-
-Copyright (c) 2011 SRI International
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
-rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
-persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
-Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
-WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
-
-/*
- * Because Internet Explorer does not support Array.indexOf(), we can add
- * it in so that subsequent calls do not break.
- *
- * @param {Object} obj
+ * Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2011 SRI International
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated  documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the  Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-if (!Array.indexOf) {
-	Array.prototype.indexOf = function(obj) {
-		for (var i = 0; i < this.length; i++) {
-			if (this[i] == obj) {
-				return i;
-			}
-		}
-		return -1;
-	};
-}
 
 /**
- * Create the requestAnimationFrame function if needed. Each browser implements
- * it as d different name currently. Default to a timeout if not supported.
- * Credit to http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+ * Create the requestAnimationFrame function if needed. Each browser implements it as a different
+ * name currently. Default to a timeout if not supported. Credit to
+ * http://paulirish.com/2011/requestanimationframe-for-smart-animating/
  * and others...
  */
 if (!window.requestAnimationFrame) {
@@ -56,39 +40,32 @@ if (!window.requestAnimationFrame) {
 
 /**
  * @namespace The core Hemi library used by Kuda.
- * @version 1.5.0
+ * @version 2.0.0-beta
  */
 var hemi = (function(hemi) {
-	
-	var errCallback = null,
-	
-		fps = 60,
-		
-		hz = 1 / fps,
-	
+
 		/*
-		 * The time of the last render in seconds.
-		 * @type {number}
+		 * Get the supported renderer for the browser (WebGL or canvas) If WebGL is not supported,
+		 * display a warning message.
+		 * 
+		 * @param {Object} element DOM element to add warning message to if necessary
+		 * @return {THREE.WebGLRenderer} the supported renderer or null
 		 */
-		lastRenderTime = 0,
-	
-		renderListeners = [],
-		
-		getRenderer = function(element) {
+	var getRenderer = function(element) {
 			var renderer = null;
-			
+
 			if (Detector.webgl) {
 				renderer = new THREE.WebGLRenderer();
 			} else {
 				if (Detector.canvas) {
 					renderer = new THREE.CanvasRenderer();
 				}
-				
+
 				Detector.addGetWebGLMessage({
 					id: 'warn_' + element.id,
 					parent: element
 				});
-				
+
 				(function(elem) {
 					setTimeout(function() {
 						var msg = document.getElementById('warn_' + elem.id);
@@ -96,126 +73,127 @@ var hemi = (function(hemi) {
 					}, 5000);
 				})(element);
 			}
-			
+
 			return renderer;
 		},
-		
+		/*
+		 * The render function to be executed on each animation frame. Calls onRender for each
+		 * render listener and then for each Client.
+		 * 
+		 * @param {boolean} update flag to force Clients to render
+		 */
 		render = function(update) {
 			requestAnimationFrame(render);
-			
-			var renderTime = new Date().getTime() * 0.001,
+
+			var renderTime = new Date().getTime(),
 				event = {
 					elapsedTime: hz
 				};
-			
-			while (renderTime - lastRenderTime > hz) {
+
+			while (renderTime - lastRenderTime > hzMS) {
 				update = true;
-				lastRenderTime += hz;
-				
-				for (var i = 0; i < renderListeners.length; ++i) {
+				lastRenderTime += hzMS;
+
+				for (var i = 0, il = renderListeners.length; i < il; ++i) {
 					renderListeners[i].onRender(event);
 				}
 			}
-			
+
 			if (update) {
-				for (var i = 0; i < hemi.clients.length; ++i) {
+				for (var i = 0, il = hemi.clients.length; i < il; ++i) {
 					hemi.clients[i].onRender(event);
 				}
 			}
 		},
-		
+		/*
+		 * Window resize handler function.
+		 */
 		resize = function() {
 			for (var i = 0; i < hemi.clients.length; ++i) {
 				hemi.clients[i]._resize();
 			}
-		};
-	
+		},
+		/*
+		 * The function to pass errors thrown using hemi.error. Default is to throw a new Error.
+		 * @type function(string):void
+		 */
+		errCallback = null,
+		/*
+		 * The current frames per second that are enforced by Hemi.
+		 * @type number
+		 * @default 60
+		 */
+		fps = 60,
+		/*
+		 * Cached inverse of the frames per second.
+		 * @type number
+		 */
+		hz = 1 / fps,
+		/*
+		 * Cached inverse of the frames per millisecond. (Internal time is in milliseconds)
+		 * @type number
+		 */
+		hzMS = hz * 1000,
+		/*
+		 * The time of the last render in milliseconds.
+		 * @type {number}
+		 */
+		lastRenderTime = 0,
+		/*
+		 * Array of render listener objects that all have an onRender function.
+		 * @type Object[]
+		 */
+		renderListeners = [];
+
+	// Useful constants to cache
+
 	/**
-	 * The version of Hemi released: 10/11/11
+	 * Conversion factor for degrees to radians.
+	 * @type number
+	 * @default Math.PI / 180
+	 */
+	hemi.DEG_TO_RAD = Math.PI / 180;
+
+	/**
+	 * Half of Pi
+	 * @type number
+	 * @default Math.PI / 2
+	 */
+	hemi.HALF_PI = Math.PI / 2;
+
+	/**
+	 * Conversion factor for radians to degrees.
+	 * @type number
+	 * @default 180 / Math.PI
+	 */
+	hemi.RAD_TO_DEG = 180 / Math.PI;
+
+	/**
+	 * The version of Hemi released: TBD
 	 * @constant
 	 */
-	hemi.version = '1.5.0';
+	hemi.version = '2.0.0-beta';
 	hemi.console.setEnabled(true);
-	
+
 	/**
 	 * The list of Clients being rendered on the current webpage.
 	 */
 	hemi.clients = [];
-	
-	/**
-	 * Search the webpage for any divs with an ID starting with "kuda" and
-	 * create a Client and canvas within each div that will be rendered to using
-	 * WebGL.
-	 */
-	hemi.makeClients = function() {
-		var elements = document.getElementsByTagName('div'),
-			numClients = hemi.clients.length;
-		
-		for (var i = 0; i < elements.length; ++i) {
-			var element = elements[i];
-			
-			if (element.id && element.id.match(/^kuda/)) {
-				var renderer = getRenderer(element);
-				
-				if (renderer) {
-					var client = i < numClients ? hemi.clients[i] : new hemi.Client(true);
-					
-					element.appendChild(renderer.domElement);
-					client.setRenderer(renderer);
-					hemi.hudManager.addClient(client);
-				}
-			}
-		}
-		
-		hemi.init();
-		return hemi.clients;
-	};
 
 	/**
-	 * Initialize hemi features. This does not need to be called if
-	 * hemi.makeClients() is called, but it can be used on its own if you don't
-	 * want to use hemi's client system.
-	 */
-	hemi.init = function() {
-		window.addEventListener('resize', resize, false);
-		lastRenderTime = new Date().getTime() * 0.001;
-		render(true);
-	};
-	
-	/**
-	 * Add the given render listener to hemi. A listener must implement the
-	 * onRender function.
+	 * Add the given render listener to hemi. A listener must implement the onRender function.
 	 * 
 	 * @param {Object} listener the render listener to add
 	 */
 	hemi.addRenderListener = function(listener) {
-		var ndx = renderListeners.indexOf(listener);
-		
-		if (ndx === -1) {
+		if (renderListeners.indexOf(listener) === -1) {
 			renderListeners.push(listener);
 		}
 	};
 
 	/**
-	 * Remove the given render listener from hemi.
-	 * 
-	 * @param {Object} listener the render listener to remove
-	 * @return {Object} the removed listener if successful or null
-	 */
-	hemi.removeRenderListener = function(listener) {
-		var ndx = renderListeners.indexOf(listener),
-			retVal = null;
-		
-		if (ndx !== -1) {
-			retVal = renderListeners.splice(ndx, 1)[0];
-		}
-
-		return retVal;
-	};
-	
-	/**
-	 * Pass the given error message to the registered error handler or throw an
-	 * Error if no handler is registered.
+	 * Pass the given error message to the registered error handler or throw an Error if no handler
+	 * is registered.
 	 * 
 	 * @param {string} msg error message
 	 */
@@ -230,15 +208,79 @@ var hemi = (function(hemi) {
 	};
 
 	/**
+	 * Get the current frames-per-second that will be enforced for rendering.
+	 * 
+	 * @return {number} current frames-per-second
+	 */
+	hemi.getFPS = function() {
+		return fps;
+	};
+
+	/**
 	 * Get the time that the specified animation frame occurs at.
 	 *
 	 * @param {number} frame frame number to get the time for
-	 * @return {number} time that the frame occurs at
+	 * @return {number} time that the frame occurs at in seconds
 	 */
 	hemi.getTimeOfFrame = function(frame) {
 		return frame * hz;
 	};
-	
+
+	/**
+	 * Initialize hemi features. This does not need to be called if hemi.makeClients() is called,
+	 * but it can be used on its own if you don't want to use hemi's client system.
+	 */
+	hemi.init = function() {
+		window.addEventListener('resize', resize, false);
+		lastRenderTime = new Date().getTime();
+		render(true);
+	};
+
+	/**
+	 * Search the webpage for any divs with an ID starting with "kuda" and create a Client and
+	 * canvas within each div that will be rendered to using WebGL.
+	 */
+	hemi.makeClients = function() {
+		var elements = document.getElementsByTagName('div'),
+			numClients = hemi.clients.length;
+		
+		for (var i = 0, il = elements.length; i < il; ++i) {
+			var element = elements[i];
+
+			if (element.id && element.id.match(/^kuda/)) {
+				var renderer = getRenderer(element);
+
+				if (renderer) {
+					var client = i < numClients ? hemi.clients[i] : new hemi.Client(true);
+
+					element.appendChild(renderer.domElement);
+					client.setRenderer(renderer);
+					hemi.hudManager.addClient(client);
+				}
+			}
+		}
+
+		hemi.init();
+		return hemi.clients;
+	};
+
+	/**
+	 * Remove the given render listener from hemi.
+	 * 
+	 * @param {Object} listener the render listener to remove
+	 * @return {Object} the removed listener if successful or null
+	 */
+	hemi.removeRenderListener = function(listener) {
+		var ndx = renderListeners.indexOf(listener),
+			retVal = null;
+
+		if (ndx !== -1) {
+			retVal = renderListeners.splice(ndx, 1)[0];
+		}
+
+		return retVal;
+	};
+
 	/**
 	 * Set the given function as the error handler for Hemi errors.
 	 * 
@@ -246,15 +288,6 @@ var hemi = (function(hemi) {
 	 */
 	hemi.setErrorCallback = function(callback) {
 		errCallback = callback;
-	};
-	
-	/**
-	 * Get the current frames-per-second that will be enforced for rendering.
-	 * 
-	 * @return {number} current frames-per-second
-	 */
-	hemi.getFPS = function() {
-		return fps;
 	};
 
 	/**
@@ -265,12 +298,8 @@ var hemi = (function(hemi) {
 	hemi.setFPS = function(newFps) {
 		fps = newFps;
 		hz = 1/fps;
+		hzMS = hz * 1000;
 	};
 
-	// Useful constants to cache
-	hemi.DEG_TO_RAD = Math.PI / 180;
-	hemi.HALF_PI = Math.PI / 2;
-	hemi.RAD_TO_DEG = 180 / Math.PI;
-	
 	return hemi;
 })(hemi || {});
