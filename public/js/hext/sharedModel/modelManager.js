299ee6efece7811e9d7e97fc00450ccce868794f
Peter Blasco <pblasco@gmail.com>
2012-01-18 16:00:19 -0800

    Updated the model manager to use new hemi

--- public/js/hext/sharedModel/modelManager.js
+++ public/js/hext/sharedModel/modelManager.js
@@ -18,6 +18,8 @@
 var hext = (function(hext) {
 	
 	hext.sharedModel = hext.sharedModel || {};
+
+	var colladaLoader = new THREE.ColladaLoader();
 	
 	/**
 	 * Creates a model manager object. Object is a singleton and always exists
@@ -34,36 +36,6 @@ var hext = (function(hext) {
 		
 		return window.parent.kuda.modelManager;
 	};
-
-	/**
-	 * Overwrites the o3d.scene.loadScene method to make use of the model 
-	 * manager.
-	 * 
-	 * @param {!o3d.Client} client An O3D client object.
-	 * @param {!o3d.Pack} pack Pack to load scene into.
-	 * @param {!o3d.Transform} parent Transform to parent scene under.
-	 * @param {string} url URL of scene to load.
-	 * @param {!function(!o3d.Pack, !o3d.Transform, *): void} callback
-	 *     Callback when scene is loaded. It will be passed the pack, the parent and
-	 *     an exception which is null on success.
-	 * @param {!o3djs.serialization.Options} opt_options Options passed into the
-	 *     loader.
-	 * @return {!o3djs.io.LoadInfo} A LoadInfo for tracking progress.
-	 * @see o3djs.loader.createLoader
-	 */
-	o3djs.scene.loadScene = function(client, 
-									 pack, 
-									 parent, 
-									 url, 
-									 callback, 
-									 opt_options){
-					 			
-		var mgr = hext.sharedModel.getModelManager(),
-			archiveInfo = mgr.addModel(url, o3djs, client, pack, parent, 
-				callback, opt_options);
-		
-		return archiveInfo;
-	};
 	
 	/**
 	 * The ModelManager manages all models, ensuring one copy per model url so
@@ -77,73 +49,45 @@ var hext = (function(hext) {
 		/**
 		 * Adds a model to the list of those being managed. If the model already
 		 * exists (identified by the url) and hasn't been loaded yet, this adds 
-		 * the caller to the list (identified by the context provided by o3d, 
-		 * client, pack, parent, and callback). If the model has already been
+		 * the caller to the list. If the model has already been
 		 * loaded, this immediately begins the deserialization process.
 		 * 
 		 * @param {string} url the path to the model file
-		 * @param {!o3djs} o3d the o3djs context object. Since different
-		 * 		contexts can be used here, this ensures the right one gets tied
-		 * 		to the caller.
-		 * @param {!o3d.Client} client the current O3D client. Required for 
-		 * 		deserialization.
-		 * @param {!o3d.Pack} pack the pack object used for creating certain o3d
-		 * 		objects. Required for deserialization and archive loading.
-		 * @param {!o3d.Transform} parent the parent transform to attach to.
-		 * @param {!function(!o3d.Pack, !o3d.Transform, *): void} callback 
-		 * 		method called when deserialization is finished.
-		 * @param {!o3djs.serialization.Options} options optional options passed 
-		 * 		to the deserializer.
+		 * @param {hemi.Client} client the current hemi client. R
+		 * @param {function(hemi.model)} callback the function to be called when the model is loaded
 		 */
-		addModel: function(url, o3d, client, pack, parent, callback, options) {
-			var	name = getName(url),
-				obj = this.models.get(name),
-				that = this;
+		addModel: function(url, client, callback) {
+			var	obj = this.models.get(url),
+				that = this,
+				thatURL = url;
 		
 			if (obj) {
-				if (obj.archiveInfo) {
-					o3d.serialization.deserializeArchive(obj.archiveInfo,
-						'scene.json', client, pack, parent,
-						callback, options);
+				if (obj.data) {
+					var collada = colladaLoader.parse(obj.data, null, url);
+					var model = new hemi.model(obj.client);
+					model.load(collada);
+					callback(model);
 				}	
 				else {
 					var configs = obj.configs;
 					configs.push({
-						o3d: o3djs,
 						client: client,
-						pack: pack,
-						parent: parent,
-						callback: callback,
-						options: options
+						callback: callback
 					});
 				}
-				return obj.loadInfo;
 			}
 			else {
-				var loadInfo = o3djs.io.loadArchive(pack, url, function(archiveInfo, exception) {
-					if (!exception) {
-						that.notifyLoaded(archiveInfo);
-					}
-					else {
-						archiveInfo.destroy();
-						callback(pack, parent, exception);
-					}
-				});
+				hemi.utils.get(url, function(data, status) {
+					that.notifyLoaded(thatURL, data);
+				}, true);
 				
-				this.models.put(name, {
+				this.models.put(url, {
 					configs: [{
-						o3d: o3djs,
 						client: client,
-						pack: pack,
-						parent: parent,
-						callback: callback,
-						options: options
+						callback: callback
 					}],
-					loadInfo: loadInfo,
-					archiveInfo: null
+					data: null
 				});
-				
-				return loadInfo;
 			}
 		},
 		
@@ -151,31 +95,25 @@ var hext = (function(hext) {
 		 * Goes through the list of callees associated with the url found in
 		 * archiveInfo, and begins the deserialization process for each callee.
 		 * 
-		 * @param {!o3djs.io.ArchiveInfo} archiveInfo the archiveInfo object 
+		 * @param {string} url the url of the model
+		 * @param {string} data the archiveInfo object 
 		 * 		created when an archive is loaded.
 		 */
-		notifyLoaded: function(archiveInfo) {
-			var url = archiveInfo.request_.uri,
-				modelObj = this.models.get(getName(url)),
+		notifyLoaded: function(url, data) {
+			var modelObj = this.models.get(url),
 				list = modelObj.configs;
 			
 			for (var ndx = 0, len = list.length; ndx < len; ndx++) {
-				var config = list[ndx],
-					o3dContext = config.o3d;
+				var config = list[ndx];
 					
-				o3dContext.serialization.deserializeArchive(archiveInfo,
-					'scene.json', config.client, config.pack, config.parent,
-					config.callback, config.options);
+				var collada = colladaLoader.parse(data, null, url);
+				var model = new hemi.Model(config.client);
+				model.load(collada);
+				config.callback(model);
 			}
 			
-			modelObj.archiveInfo = archiveInfo;
+			modelObj.data = data;
 		}
-	};
-	
-	var getName = function(url) {		
-		var arr = url.split('/');
-		arr = arr[arr.length - 2];
-		return arr;
 	};
 	
 	return hext;
