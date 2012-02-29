3a4f1ce22d94df1e4c670d787ad674f720c95c16
Erik Kitson <erik.kitson@gmail.com>
2012-02-29 13:49:39 -0800

    Fixed Curve octane mo betta.

--- public/js/hemi/curve.js
+++ public/js/hemi/curve.js
@@ -292,9 +292,10 @@
 	 */
 	Curve.prototype._octane = function() {
 		var names = ['count', 'tension', 'weights'],
-			props = [];
+			props = [],
+			i, il;
 
-		for (var i = 0, il = names.length; i < il; ++i) {
+		for (i = 0, il = names.length; i < il; ++i) {
 			var name = names[i];
 
 			props.push({
@@ -303,15 +304,22 @@
 			});
 		}
 
-		props.push({
-			name: 'points',
-			oct: this.points
-		});
+		names = ['points', 'tangents'];
 
-		props.push({
-			name: 'tangents',
-			oct: this.points
-		});
+		for (i = 0, il = names.length; i < il; ++i) {
+			var name = names[i],
+				prop = this[name],
+				oct = [];
+
+			for (var j = 0, jl = prop.length; j < jl; ++j) {
+				oct.push(prop[j]._toOctane());
+			}
+
+			props.push({
+				name: name,
+				oct: oct
+			});
+		}
 
 		props.push({
 			name: 'setType',
